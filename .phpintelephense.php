44ab4e83fe42a8c82b44cb8967d34cd69ecf659e
Domok23 <wahyur2bro@gmail.com>
2026-05-22 02:48:36 +0700

    Add .phpintelephense.php configuration for Laravel stubs

---
+++ .phpintelephense.php
@@ -0,0 +1,8 @@
+{
+    "stubs": {
+        "include": [
+            "vendor/laravel/framework/src/Illuminate/Database/Migrations/*.php",
+            "vendor/laravel/framework/src/Illuminate/Database/Schema/*.php"
+        ]
+    }
+}
