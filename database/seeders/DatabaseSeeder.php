58304881a821668d6795dc7371bc486b0e256af1
Domok23 <wahyur2bro@gmail.com>
2026-07-22 04:34:06 +0700

    feat(hr): add HrModuleSeeder to database seeder
    Include HrModuleSeeder in the DatabaseSeeder to ensure HR-related data is seeded during the database initialization process.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             CompanySeeder::class,
             AdminUserSeeder::class,
             DataSeeder::class,
+            HrModuleSeeder::class,
             ChartOfAccountSeeder::class,
             GeneralLedgerSeeder::class,
         ]);
